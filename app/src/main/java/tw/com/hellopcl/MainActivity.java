9316eea8c04081a28ccab634817fd19ace6987e7
andophine <31679181+andophine@users.noreply.github.com>
2018-08-01 17:37:03 +0800

    Add files via upload
    after download source, please copy the installable files of pcl-for-android (or from super build), include boost-android, flann-android, pcl-android and eigen into app/libs folder.

---
+++ app/src/main/java/tw/com/hellopcl/MainActivity.java
@@ -0,0 +1,31 @@
+package tw.com.hellopcl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import tw.com.hellpcl.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("JNIHelloPCL");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+        boostMkDir();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String boostMkDir();
+}
